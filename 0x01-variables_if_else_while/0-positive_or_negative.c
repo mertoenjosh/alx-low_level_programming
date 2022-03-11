d3df98dc50e8c616f2eb5cb2cbbc960473ad495d
mertoenjosh <mertoenjosh3527@gmail.com>
2022-03-11 20:23:30 +0300

    automated_push

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@ int main(void)
   if (n > 0) {
     printf("%d is positive\n", n);
   } else if (n < 0) {
-    printf("%d\n is negative", n);
+    printf("%d is negative\n", n);
   } else {
     printf("%d is zero\n", n);
   }

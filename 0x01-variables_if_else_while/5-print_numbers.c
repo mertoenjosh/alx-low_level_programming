c0adb9fdbdd6a85dbb4f25332e57b3f58837e584
mertoenjosh <mertoenjosh3527@gmail.com>
2022-03-11 22:40:35 +0300

    automated_push

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}

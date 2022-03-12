97c8f6dc3521fc390602f59422935d728f096d91
mertoenjosh <mertoenjosh3527@gmail.com>
2022-03-12 09:26:16 +0300

    automated_push

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - use puts to print
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char x[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(x);
+	return (0);
+}

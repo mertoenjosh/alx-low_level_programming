3d173a11439e9dbad7b37159fced4ee621b33332
mertoenjosh <mertoenjosh3527@gmail.com>
2022-03-15 15:26:43 +0300

    automated_push

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,12 +8,21 @@ void times_table(void)
 {
 	int i, j, tot;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 1; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			tot = i * j;
-			_putchar(tot);
+			if (tot <= 9)
+			{
+				_putchar(32);
+				_putchar(tot + 48);
+			}
+			else
+			{
+				_putchar((tot / 10) + 48);
+				_putchar((tot  % 10) + 48);
+			}
 		}
 		_putchar('\n');
 	}

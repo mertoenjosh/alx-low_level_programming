c277dc2435b9d923fb81804d253c859221d23819
mertoenjosh <mertoenjosh3527@gmail.com>
2022-03-15 16:11:09 +0300

    automated_push

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,16 +10,9 @@ void print_to_98(int n)
 {
 	if (n > 98)
 		for (; n >= 98; n--)
-			if (n != 98)
-				printf("%d, ", n);
-			else
-				printf("%d", n);
+			printf("%d, ", n);
 	else if (n < 98)
 		for (; n <= 98; n++)
-			if (n != 98)
-				printf("%d, ", n);
-			else
-				printf("%d", n);
-	else
-		printf("%d\n", n);
+			printf("%d, ", n);
+	printf("%d\n", n);
 }

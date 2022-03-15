a201f69478901f38e9bcb466d8a4dab9db20de41
mertoenjosh <mertoenjosh3527@gmail.com>
2022-03-15 14:46:03 +0300

    automated_push

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+void print_alphabet_x10(void)
+{
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (i = 1; i <= 26; i++)
+		{
+			_putchar(alph[i]);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}

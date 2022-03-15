fa603a803cba9ce9761ad2bd2126452d793966bb
mertoenjosh <mertoenjosh3527@gmail.com>
2022-03-15 15:51:43 +0300

    automated_push

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#inlude "main.h"
+
+/**
+ * add - returns sum of two
+ * @x: the first num
+ * @y: the second num
+ *
+ * Return: The sum oy x and y
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}

46303d77c4ba7360c22c61a41e3a11b6054eb7b9
mertoenjosh <mertoenjosh3527@gmail.com>
2022-03-17 22:28:55 +0300

    automated_push

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (i > 9)
+			if (j > 9)
 			{
-				_putchar((i / 10) + '0');
+				_putchar((j / 10) + 48);
 			}
-			_putchar((i % 10) + '0');
+			_putchar((j % 10) + 48);
 		}
 		_putchar('\n');
 	}

0d2301ef39d5666d25b60e9f77021bcbd8e40289
mertoenjosh <mertoenjosh3527@gmail.com>
2022-03-23 21:57:39 +0300

    copy upto nth char

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy upto nth char
+ *
+ * @dest: The destination string
+ * @src: The source string
+ * @n: the nth char
+ *
+ * Return: the copied string i.e dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+

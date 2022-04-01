269f41b6760fd779d1156f7fe86505003f71684e
mertoenjosh <mertoenjosh3527@gmail.com>
2022-04-01 21:43:58 +0300

    prints number of args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,11 +3,15 @@
 /**
  * main - print number of args passed
  *
+ * @argc: number of args
+ * @argv: array of the passed args
+ *
  * Return: 0
  */
 
 int main(int argc, char __attribute__((unused)) **argv)
 {
-	_putchar('0' + argc);
+	_putchar('0' + (argc - 1));
+	_putchar('\n');
 	return (0);
 }

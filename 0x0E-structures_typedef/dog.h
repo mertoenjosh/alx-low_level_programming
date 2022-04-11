0b0d148927e4ba7a020bd20a3ea0b6f70ce502c2
mertoenjosh <mertoenjosh3527@gmail.com>
2022-04-11 12:33:45 +0300

    created a dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * struct dog - dog features
+ * @name: the name of the dog
+ * @age: how old is the dog
+ * @owner: the name of the owner
+ *
+ * Description: The structure should define properties of a dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif

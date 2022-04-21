38062f9fb1eeaec264b29ca0df2bab64707f323f
mertoenjosh <mertoenjosh3527@gmail.com>
2022-04-21 19:50:58 +0300

    a function that prints all the elements of a node

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a list_t list
+ *
+ * @h: the node passed
+ * Return: num of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	char *s;
+	unsigned int i = 0, len = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			s = "(nil)";
+		}
+		else
+		{
+			s = h->str;
+			len = h->len;
+		}
+
+		printf("[%u] %s\n", len, s);
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}

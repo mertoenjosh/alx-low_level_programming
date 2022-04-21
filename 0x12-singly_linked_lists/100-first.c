7289675cdb0a8a01e8ab4b12de346352b7452eca
mertoenjosh <mertoenjosh3527@gmail.com>
2022-04-22 01:09:35 +0300

    execute a function before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * first - executed before main
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}

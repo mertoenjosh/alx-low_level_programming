832f81ecdde998cf119df620f57c1fdd58aad25f
mertoenjosh <mertoenjosh3527@gmail.com>
2022-04-25 19:45:48 +0300

    free node, set head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - sets the head to NULL
+ *
+ * @head: head of the list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	while (*head != NULL)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node);
+	}
+	*head = NULL;
+}

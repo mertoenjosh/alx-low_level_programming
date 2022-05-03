b740717d12cb9d86cd67a77980ed96fce301eef6
mertoenjosh <mertoenjosh3527@gmail.com>
2022-05-03 23:25:43 +0300

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,9 +30,10 @@ int create_file(const char *filename, char *text_content)
 
 	fd_wr = write(fd, text_content, len);
 
+	close(fd);
+
 	if (fd_wr == -1)
 		return (-1);
 
-	close(fd);
 	return (1);
 }
